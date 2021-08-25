ea9a011354b48e390a831682d556cc986ab4e5e3
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-25 16:30:47 +0300

    Fix

---
+++ src/js/Open Server/domains/WebApplication/src/ts/app.js
@@ -0,0 +1,13 @@
+﻿import lib from "./lib";
+import Greeter from "./main";
+//global.jQuery = global.$ = $;
+//window.console.log($);
+// ts/app.ts
+window.console.log("ts/app.ts");
+window.console.log(lib);
+window.onload = () => {
+    const el = document.body;
+    const greeter = new Greeter(el);
+    greeter.start();
+};
+//# sourceMappingURL=app.js.map
