22cefb0e58f7269da1e3d13b723f0b806026d10f
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-25 15:16:39 +0300

    Fix

--- src/js/ts/app.js
+++ src/js/ts/app.js
@@ -1,5 +1,8 @@
 ﻿import lib from "./lib";
 import Greeter from "./main";
+//import $ from "jquery";
+// global.jQuery = global.$ = $;
+//window.console.log($);
 // ts/app.ts
 window.console.log("ts/app.ts");
 window.console.log(lib);
