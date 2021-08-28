70f47892d17a8b374448d49afc716a13ce7d79f7
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-28 19:29:14 +0300

    Fix

--- gulpfile-template.js
+++ gulpfile-template.js
@@ -1,27 +1,28 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
 
-const server = require('./gulp/tasks/server'),
-	pug2html = require('./gulp/tasks/pug2html'),
-	styles = require('./gulp/tasks/styles'),
-	scripts = require('./gulp/tasks/scripts'),
-	fonts = require('./gulp/tasks/fonts'),
-	imageMinify = require('./gulp/tasks/imageMinify'),
-	clean = require('./gulp/tasks/clean'),
-	copyDependencies = require('./gulp/tasks/copyDependencies'),
-	lighthouse = require('./gulp/tasks/lighthouse'),
-	svgSprite = require('./gulp/tasks/svgSprite');
+const server = require('./gulp/tasks/server')
+const pug2html = require('./gulp/tasks/pug2html')
+const styles = require('./gulp/tasks/styles')
+const scripts = require('./gulp/tasks/scripts')
+const fonts = require('./gulp/tasks/fonts')
+const imageMinify = require('./gulp/tasks/imageMinify')
+const clean = require('./gulp/tasks/clean')
+const copyDependencies = require('./gulp/tasks/copyDependencies')
+const lighthouse = require('./gulp/tasks/lighthouse')
+const svgSprite = require('./gulp/tasks/svgSprite')
 
-function setMode(isProd = false) {
-	return cb => {
-		process.env.NODE_ENV = isProd ? 'production' : 'development';
-		cb();
-	};
+function setMode(isProduction = false) {
+  return cb => {
+    process.env.NODE_ENV = isProduction ? 'production' : 'development'
+    cb()
+  }
 }
 
-const dev = gulp.parallel(pug2html, styles, scripts, fonts, imageMinify, svgSprite),
-	build = gulp.series(clean, copyDependencies, dev);
+const dev = gulp.parallel(pug2html, styles, scripts, fonts, imageMinify, svgSprite)
 
-module.exports.start = gulp.series(setMode(), build, server);
-module.exports.build = gulp.series(setMode(true), build);
+const build = gulp.series(clean, copyDependencies, dev)
 
-module.exports.lighthouse = gulp.series(lighthouse);
+module.exports.start = gulp.series(setMode(), build, server)
+module.exports.build = gulp.series(setMode(true), build)
+
+module.exports.lighthouse = gulp.series(lighthouse)
