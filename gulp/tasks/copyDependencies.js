ffcdb3f1cbcb197b1829df0150166594e52210da
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-28 18:16:11 +0300

    Fix

--- gulp/tasks/copyDependencies.js
+++ gulp/tasks/copyDependencies.js
@@ -6,8 +6,8 @@ const del = require('del')
 const config = require('../gulpfile-template.config')
 
 module.exports = function copyModules(cb) {
-  del(config.copyDependencies.dist).then(() => {
-    gulp.src(npmDist(), { base: path.join(config.root, 'node_modules') })
-      .pipe(gulp.dest(config.copyDependencies.dist)).on('end', cb)
-  }).catch(cb)
+	del(config.copyDependencies.dist).then(() => {
+		gulp.src(npmDist(), { base: path.join(config.build, 'node_modules') })
+			.pipe(gulp.dest(config.copyDependencies.dist)).on('end', cb)
+	}).catch(cb)
 }
