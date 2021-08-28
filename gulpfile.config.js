d6f1ea773af8aab6123124ea2f88d95ce40b7b1f
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-28 19:29:14 +0300

    Fix

--- gulpfile.config.js
+++ gulpfile.config.js
@@ -6,28 +6,65 @@ const { join } = require('path'),
 	port = process.env.PORT || 8080,
 	customDomain = process.env.DEV_DOMAIN ? process.env.DEV_DOMAIN : 'localhost'; // environment = process.env
 
-const root = __dirname;
-const build = path.resolve(root, 'build');
-const src = path.resolve(root, 'src');
+const root = __dirname,
+	build = path.resolve(root, 'build'),
+	src = path.resolve(root, 'src');
 
 module.exports = {
 	root,
 	build,
 	src,
-	pug2html: {
-		beautifyHtml: false
-	},
-	lighthouse: {
-		reportPath: path.resolve(root, 'reports'),
-		PORT: 8080,
-		chromeLauncherPort: 9222,
-		config: {
-			extends: 'lighthouse:default'
+	// конфигурация browser sync
+	serverConfig: {
+		server: {
+			baseDir: build,
+			index: 'index.html'
 		},
-		flags: {
-			// available options - https://github.com/GoogleChrome/lighthouse/#cli-options
-			chromeFlags: ['--show-paint-rects'],
-			output: 'html'
+		tunnel: true,
+		host: 'localhost',
+		port: 7787,
+		logPrefix: "WebDev"
+	},
+	site: {
+		server: 'site.ru',
+		user: 'tstv',
+		pass: '112121',
+		port: '10000',
+		folder: ''
+	},
+	paths: {
+		build: { // пути для сборки проектов
+			all: build,
+			html: build + 'html/',
+			css: build + 'css/',
+			js: build + 'js/',
+			favicon: build + 'favicon/',
+			faviconDataFile: src + 'favicon/faviconData.json',
+			faviconInject: build + '**/*.html',
+			img: build + 'img/'
+		},
+		src: { // пути размещения исходных файлов проекта
+			all: src,
+			html: src + 'html/**/*.{html,htm}',
+			pug: src + 'pug/*.pug',
+			scss: src + 'scss/*.scss',
+			js: src + 'js/*.{js,js.map}',
+			webpack: src + 'js/**/*.js',
+			favicon: src + 'favicon/icon.png',
+			iconsPath: '/favicon',
+			img: src + 'img/**/*.{jpeg,jpg,png,svg,gif}'
+		},
+		watch: { // пути файлов, за изменением которых мы хотим наблюдать
+			html: src + 'html/**/*.{html,htm}',
+			scss: src + 'scss/**/*.scss',
+			js: src + 'js/**/*.js'
+		},
+		clean: { // путь очистки директории для сборки
+			build: build + '**/*',
+			html: build + 'html',
+			css: build + 'css',
+			js: build + 'js',
+			webpack: build + 'webpack'
 		}
 	},
 	copyDependencies: {
