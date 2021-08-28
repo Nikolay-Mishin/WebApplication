cbef4aa79753435df9ac0de7b70205b6edfaa493
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-28 18:59:17 +0300

    Fix

---
+++ tasks/pug2html.js
@@ -0,0 +1,17 @@
+const config = require('../gulpfile-template.config'),
+	gulp = require('gulp'),
+	plumber = require('gulp-plumber'),
+	pug = require('gulp-pug'),
+	pugLinter = require('gulp-pug-linter'),
+	htmlValidator = require('gulp-w3c-html-validator'),
+	bemValidator = require('gulp-html-bem-validator');
+
+module.exports = function pug2html() {
+	return gulp.src('src/pages/*.pug')
+		.pipe(plumber())
+		.pipe(pugLinter({ reporter: 'default' }))
+		.pipe(pug({ pretty: config.pug2html.beautifyHtml }))
+		.pipe(htmlValidator())
+		.pipe(bemValidator())
+		.pipe(gulp.dest('build'));
+};
