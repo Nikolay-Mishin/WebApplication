e57a85104fd4a84c8c2fa5e74c76c6d22118e9c7
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-28 20:35:52 +0300

    Fix

---
+++ gulpfile - Копировать - Копировать.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+const root = __dirname,
+	build = path.resolve(root, 'build'),
+	src = path.resolve(root, 'src');
+
+module.exports = {
+	root,
+	build,
+	src,
+	// конфигурация browser sync
+	serverConfig: {
+		server: {
+			baseDir: build,
+			index: 'index.html'
+		},
+		tunnel: true,
+		host: 'localhost',
+		port: 7787,
+		logPrefix: "WebDev"
+	},
+	site: {
+		server: 'site.ru',
+		user: 'tstv',
+		pass: '112121',
+		port: '10000',
+		folder: ''
+	},
+	paths: {
+		build: { // пути для сборки проектов
+			all: build,
+			html: build + 'html/',
+			css: build + 'css/',
+			js: build + 'js/',
+			favicon: build + 'favicon/',
+			faviconDataFile: src + 'favicon/faviconData.json',
+			faviconInject: build + '**/*.html',
+			img: build + 'img/'
+		},
+		src: { // пути размещения исходных файлов проекта
+			all: src,
+			html: src + 'html/**/*.{html,htm}',
+			pug: src + 'pug/*.pug',
+			scss: src + 'scss/*.scss',
+			js: src + 'js/*.{js,js.map}',
+			webpack: src + 'js/**/*.js',
+			favicon: src + 'favicon/icon.png',
+			iconsPath: '/favicon',
+			img: src + 'img/**/*.{jpeg,jpg,png,svg,gif}'
+		},
+		watch: { // пути файлов, за изменением которых мы хотим наблюдать
+			html: src + 'html/**/*.{html,htm}',
+			scss: src + 'scss/**/*.scss',
+			js: src + 'js/**/*.js'
+		},
+		clean: { // путь очистки директории для сборки
+			build: build + '**/*',
+			html: build + 'html',
+			css: build + 'css',
+			js: build + 'js',
+			webpack: build + 'webpack'
+		}
+	}
+};
