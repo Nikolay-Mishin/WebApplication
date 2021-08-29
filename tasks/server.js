06766de593f569240d537c48af51e2fd5879925a
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-29 17:43:43 +0300

    Fix

--- tasks/server.js
+++ tasks/server.js
@@ -1,33 +1,21 @@
-const { serverConfig } = require('./gulpfile.config'),
-	{ readyReload } = require('./_helpers'),
-	{ watch, series } = require('gulp'),
-	browserSync = require('browser-sync');
+const { watch, series } = require('gulp'),
+	{ serverConfig } = require('./gulpfile.config'),
+	//{ server } = require('./_helpers');
+	browserSync = require('browser-sync'), // плагин перезагрузки браузера
+	server = browserSync.create();
 
-const imageMinify = require('./imageMinify'),
-	svgSprite = require('./svgSprite'),
-	styles = require('./styles'),
-	pug2html = require('./pug2html'),
-	scripts = require('./scripts'),
-	copyDependencies = require('./copyDependencies');
-
-module.exports = function server(done) {
-	browserSync(serverConfig); // локальный сервер
+function reload(done) {
+	server.reload();
 	done();
+}
 
-	//server.init({
-	//	server: 'build',
-	//	notify: false,
-	//	open: true,
-	//	cors: true
-	//});
+module.exports = function server_task(done) {
+	server.init(serverConfig);
+	//browserSync(serverConfig); // локальный сервер
 
-	//watch('src/img/*.{gif,png,jpg,svg,webp}', series(imageMinify, readyReload));
-	//watch('src/img/sprite/*.svg', series(svgSprite, readyReload));
-	//watch('src/styles/**/*.scss', series(styles, done => gulp.src('build/css').pipe(server.stream()).on('end', done)));
-	//watch('src/js/**/*.js', series(scripts, readyReload));
-	//watch('src/pages/**/*.pug', series(pug2html, readyReload));
+	//watch(paths.watch.html, series('dev:html'), reload);
+	//watch(paths.watch.scss, series('dev:scss'), reload);
+	//watch(paths.watch.js, series('dev:js'), reload);
 
-	//watch('package.json', series(copyDependencies, readyReload));
-
-	//return done();
+	done();
 };
