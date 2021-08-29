e3005fec15ef151b38d1c331761f6319d0762d5b
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-29 21:57:37 +0300

    Fix

--- tasks/check-for-favicon-update.js
+++ tasks/check-for-favicon-update.js
@@ -2,7 +2,8 @@ const fs = require('fs'), // работа с файловой системой
 	{ paths } = require('../gulpfile.config'),
 	realFavicon = require('gulp-real-favicon'); // генератор фавиконок
 
-module.exports = function check_for_favicon_update() {
+module.exports = function check_for_favicon_update(done) {
 	let currentVersion = JSON.parse(fs.readFileSync(paths.build.faviconDataFile)).version;
 	realFavicon.checkForUpdates(currentVersion, err => { if (err) throw err; });
+	done();
 };
