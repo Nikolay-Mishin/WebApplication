b3d7f14612d7905bb27263e89f0d70e9cc29080d
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-29 21:57:37 +0300

    Fix

--- tasks/dev-img.js
+++ tasks/dev-img.js
@@ -1,10 +1,11 @@
 const { imgMinify } = require('./_helpers'); // плагин расстановки префиксов
 
-module.exports = function dev_img() {
-	return imgMinify.run(function(err, files) {
+module.exports = function dev_img(done) {
+	imgMinify.run(function(err, files) {
 		if (err) {
 			throw err;
 		}
 		console.log('Images optimized successfully!');
 	});
+	done();
 };
