d712ecc8d2efbf63df55f9a50a7aceb4b8f96174
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-29 20:33:26 +0300

    Fix

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,5 +1,6 @@
 const { src, dest } = require('gulp'),
 	{ paths } = require('../gulpfile.config'),
+	{ lastRun } = require('./_helpers'),
 	sourcemaps = require('gulp-sourcemaps'), // плагин создания map-файлов
 	rename = require('gulp-rename'), // плагин переименования файлов
 	concat = require('concat'),
@@ -8,7 +9,7 @@ const { src, dest } = require('gulp'),
 	gulpTerser = require('gulp-terser');
 
 module.exports = function scripts() {
-	return src(paths.src.js)
+	return src(paths.src.js, lastRun(scripts))
 		.pipe(sourcemaps.init())
 		.pipe(babel({
 			//presets: ['env']
