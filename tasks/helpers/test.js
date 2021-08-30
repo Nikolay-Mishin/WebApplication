0e7593bcaa1acf84b958fb7a29bfeffb54c33d1c
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-30 15:55:50 +0300

    Fix

--- tasks/helpers/test.js
+++ tasks/helpers/test.js
@@ -29,7 +29,7 @@ module.exports = function test(done) {
 
 	console.log('log\n', log);
 
-module.exports = function _test(done) {
+module.exports = function test(done) {
 	console.log(config);
 
 	//console.log(process);
