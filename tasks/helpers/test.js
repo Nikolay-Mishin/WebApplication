6fc10fdb6f4556c8160f069862f0221346389b1e
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-30 18:20:39 +0300

    Fix

--- tasks/helpers/test.js
+++ tasks/helpers/test.js
@@ -32,19 +32,24 @@ module.exports = function test(done) {
 module.exports = function test(done) {
 	console.log(config);
 
-	//console.log(process);
-	//console.log(process.env);
-	console.log(process.argv);
-	console.log(arg);
+	const log = { [h.mode]: {} };
 
-	console.log(process.env.NODE_ENV);
-	console.log('dev: ' + h.dev);
-	console.log('prod: ' + h.prod);
+	if (h.dev) {
+		log[h.mode].NODE_ENV = process.env.NODE_ENV;
+		log[h.mode].dev = h.dev;
+		log[h.mode].prod = h.prod;
+	}
 
 	setMode(true);
-	console.log(process.env.NODE_ENV);
-	console.log('dev: ' + h.dev);
-	console.log('prod: ' + h.prod);
+	log[h.mode] = {};
+
+	if (h.prod) {
+		log[h.mode].NODE_ENV = process.env.NODE_ENV;
+		log[h.mode].dev = h.dev;
+		log[h.mode].prod = h.prod;
+	}
+
+	console.log('log\n', log);
 
 	done();
 };
