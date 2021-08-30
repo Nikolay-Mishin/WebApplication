9b26fdeae11fa655c44bc665292a270b788e50bc
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-08-30 15:55:50 +0300

    Fix

--- tasks/prod-js.js
+++ tasks/prod-js.js
@@ -1,6 +1,6 @@
 const { src, dest } = require('gulp'),
 	{ paths } = require('../gulpfile.config'),
-	{ lastRun/*, server*/ } = require('./helpers/helpers'),
+	{ lastRun, error, notify/*, server*/ } = require('./helpers/helpers'),
 	reload = require('browser-sync').reload, // плагин перезагрузки браузера
 	rigger = require('gulp-rigger'), // плагин объединения js
 	uglify = require('gulp-uglify'); // плагин сжатия js
@@ -8,8 +8,9 @@ const { src, dest } = require('gulp'),
 module.exports = function prod_js() {
 	return src(paths.src.js, lastRun(prod_js))
 		.pipe(rigger()) // собрать в одном файле код из скриптов
-		.pipe(uglify()) // минификация
+		.pipe(uglify().on('error', error)) // минификация
 		.pipe(dest(paths.build.js))
+		.pipe(notify('prod:js'))
 		.pipe(reload({ stream: true })); // И перезагрузим сервер
 		//.pipe(server.stream());
 };
