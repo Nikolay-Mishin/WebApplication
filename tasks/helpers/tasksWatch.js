c46afbedb87b8223faa30e86d5101120f9b9af77
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-03 17:30:42 +0300

    Fix

--- tasks/helpers/tasksWatch.js
+++ tasks/helpers/tasksWatch.js
@@ -6,15 +6,16 @@ export default async () => {
 	console.log('root: ', root);
 	console.log('__dirname: ', process.__dirname(import.meta));
 	watch([`${root}/*.js`, `${root}/*.json`, `${root}/tasks/**/*`], function gulpfile() {
-		src([`${root}/gulpfile.js`, `${root}/*.config.js`], { since: lastRun(gulpfile) }).on('data', file => {
-			console.log(file.relative);
-		})
-		//.pipe(dest(`${root}/../_server/gulpfile`))
-		//.pipe(dest(`${root}/../../../gulpfile`));
+		src([`${root}/../../../*.package.*json`, `${root}/gulpfile.js`, `${root}/*.config.js`], { since: lastRun(gulpfile) })
+			.on('data', file => {
+				console.log(file.relative);
+			})
+			//.pipe(dest(`${root}/../_server`))
+			//.pipe(dest(`${root}/../../..`));
 		return src([`${root}/tasks/**/*`], { since: lastRun(gulpfile) }).on('data', file => {
 			console.log(`tasks/${file.relative}`);
 		})
-		//.pipe(dest(`${root}/../_server/gulpfile/tasks`))
-		//.pipe(dest(`${root}/../../../gulpfile/tasks`));
+			//.pipe(dest(`${root}/../_server/tasks`))
+			//.pipe(dest(`${root}/../../../tasks`));
 	})
 };
