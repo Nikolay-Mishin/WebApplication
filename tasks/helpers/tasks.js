c63c9e6eef9936765d019e81885d2e15e23acf9f
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-05 17:19:26 +0300

    Fix

--- tasks/helpers/tasks.js
+++ tasks/helpers/tasks.js
@@ -11,7 +11,7 @@ export default process.node_tasks = process.node_tasks || (function tasks() {
 	if (process.node_tasks) return process.node_tasks;
 	const tasks = {};
 	getFiles(tasksPath, excludeTasks).forEach(file => {
-		tasks[base(file, '.js').replace(/\-+/g, '_')] = import(`${(p = relative(__dirname, tasksPath) ? p) : '.'}/${file}`);
+		tasks[base(file, '.js').replace(/\-+/g, '_')] = import(`${(p = relative(__dirname, tasksPath)) ? p : '.'}/${file}`);
 	});
 	return process.node_tasks = tasks;
 })();
