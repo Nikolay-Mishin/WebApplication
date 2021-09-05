63ae3fad89a1d9d1c264cf65566eb34a46df22db
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-05 17:42:35 +0300

    Fix

--- tasks/helpers/tasks.js
+++ tasks/helpers/tasks.js
@@ -1,4 +1,4 @@
-import h from 'helpers.js';
+import h from './helpers.js';
 const { config, getFiles, __dirname } = h,
 	{
 		tasksPath, excludeTasks = [],
@@ -11,7 +11,8 @@ export default process.node_tasks = process.node_tasks || (function tasks() {
 	if (process.node_tasks) return process.node_tasks;
 	const tasks = {};
 	getFiles(tasksPath, excludeTasks).forEach(file => {
-		tasks[base(file, '.js').replace(/\-+/g, '_')] = import(`${(p = relative(__dirname, tasksPath)) ? p : '.'}/${file}`);
+		const path = relative(__dirname(import.meta), tasksPath);
+		tasks[base(file, '.js').replace(/\-+/g, '_')] = import(`${path ? path : '.'}/${file}`);
 	});
 	return process.node_tasks = tasks;
 })();
