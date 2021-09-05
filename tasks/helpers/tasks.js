8a4ecc53c15f0a9fe96ae625cc645090cc7ee819
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-05 14:45:15 +0300

    Fix

--- tasks/helpers/tasks.js
+++ tasks/helpers/tasks.js
@@ -10,7 +10,7 @@ module.exports = process.node_tasks = process.node_tasks || (function tasks() {
 	if (process.node_tasks) return process.node_tasks;
 	process.node_tasks = {};
 	getFiles(tasksPath, excludeTasks).forEach(file => {
-		process.node_tasks[basename(file, '.js').replace(/\-+/g, '_')] = require(`${relative(tasksPath, tasksPath)}/${file}`);
+		process.node_tasks[basename(file, '.js').replace(/\-+/g, '_')] = require(`${relative(tasksPath, __dirname)}/${file}`);
 	});
 	return process.node_tasks;
 })();
