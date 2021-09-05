290e1acf7790d767499b848cf7414b9394c05424
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-05 16:59:24 +0300

    Fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,24 @@
 
 exports._tasksWatch = require('./tasks/helpers/tasksWatch');
 
-const tasks = require('./tasks/helpers/tasks');
-//	{ setMode, modules } = require('./tasks/helpers/helpers'),
-//	{ series, parallel } = modules.gulp,
-//	{
-//		clean, html, js, dev_html, dev_scss, dev_js, dev_img, generate_favicon, server, watch, prod_html, prod_scss, prod_js,
-//		deploy: _deploy
-//	} = tasks;
+const tasks = require('./tasks/helpers/tasks'),
+	{ setMode, modules } = require('./tasks/helpers/helpers'),
+	{ series, parallel } = modules.gulp,
+	{
+		clean, html, js, dev_html, dev_scss, dev_js, dev_img, generate_favicon, server, watch, prod_html, prod_scss, prod_js,
+		deploy: _deploy
+	} = tasks;
 
 Object.assign(exports, tasks);
 
-//exports.build = series(clean, parallel(html, js));
-//exports.move = series(clean, tasks.move_files);
-//exports.dev = series(clean, setMode(), parallel(dev_html, dev_scss, dev_js, dev_img/*, generate_favicon*/), server, watch);
-//exports.prod = series(clean, setMode(true), parallel(prod_html, prod_scss, prod_js, dev_img));
-//exports.deploy = series(exports.prod, _deploy);
-//exports._test = require('./tasks/helpers/test');
+exports.build = series(clean, parallel(html, js));
+exports.move = series(clean, tasks.move_files);
+exports.dev = series(clean, setMode(), parallel(dev_html, dev_scss, dev_js, dev_img/*, generate_favicon*/), server, watch);
+exports.prod = series(clean, setMode(true), parallel(prod_html, prod_scss, prod_js, dev_img));
+exports.deploy = series(exports.prod, _deploy);
+exports._test = require('./tasks/helpers/test');
 
-//// задача по умолчанию
-////exports.default = series(exports.build);
+// задача по умолчанию
+//exports.default = series(exports.build);
 
-//console.log('exports\n', exports);
+console.log('exports\n', exports);
--- tasks/helpers/tasks.js
+++ tasks/helpers/tasks.js
@@ -10,7 +10,7 @@ module.exports = process.node_tasks = process.node_tasks || (function tasks() {
 	if (process.node_tasks) return process.node_tasks;
 	const tasks = {};
 	getFiles(tasksPath, excludeTasks).forEach(file => {
-		tasks[base(file, '.js').replace(/\-+/g, '_')] = require(`${p = relative(__dirname, tasksPath) ? p : '.'}/${file}`);
+		tasks[base(file, '.js').replace(/\-+/g, '_')] = require(`${(p = relative(__dirname, tasksPath)) ? p : '.'}/${file}`);
 	});
 	return process.node_tasks = tasks;
 })();
