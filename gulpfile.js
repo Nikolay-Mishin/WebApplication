d3b94a681d812302d5344b647da81f90cabcce7a
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-05 18:25:27 +0300

    Fix binding

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-/// <binding ProjectOpened='_tasksWatch' />
+/// <binding ProjectOpened='_tasksWatch, _test' />
 
 import _tasksWatch from './tasks/helpers/tasksWatch.js';
 import _test from './tasks/helpers/test.js';
