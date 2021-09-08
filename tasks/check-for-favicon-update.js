39c088ea7cac9bb85f24f5c183d6207f2215095f
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-08 22:25:19 +0300

    Fix

--- tasks/check-for-favicon-update.js
+++ tasks/check-for-favicon-update.js
@@ -1,9 +1,12 @@
 const {
 	config: { paths },
-	modules: { fs, realFavicon }
+	modules: {
+		fs: { readFileSync: readFile },
+		realFavicon: { checkForUpdates }
+	}
 } = require('./helpers/helpers');
 
 module.exports = async function check_for_favicon_update() {
-	let currentVersion = JSON.parse(fs.readFileSync(paths.build.faviconDataFile)).version;
-	realFavicon.checkForUpdates(currentVersion, err => { if (err) throw err; });
+	let currentVersion = JSON.parse(readFile(paths.build.faviconDataFile)).version;
+	checkForUpdates(currentVersion, err => { if (err) throw err; });
 };
