9a9def1b472e3245bd9d4674dbcfcd532bd772e7
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-11 23:32:49 +0300

    Fix

--- tasks/helpers/helpers.js
+++ tasks/helpers/helpers.js
@@ -76,8 +76,8 @@ module.exports = {
 	get getMode() { return process.env.NODE_ENV; },
 	setMode(prod = false) { return async () => this.setModeSync(prod); },
 	setModeSync(prod = false) { return process.env.NODE_ENV = prod ? 'production' : 'development'; },
-	fileName, isDir, isFile, getFiles, parseArgs, getDefaultContext, options, runInContext,
-	// filtered = Object.filter(scores, ([name, score]) => score > 1);
+	fileName, isDir, isFile, getFiles, getDefaultContext, options, runInContext,
+	// filtered = Object.filter(scores, ([key, value]) => value > 1);
 	filter: Object.filter = (obj, predicate) => Object.fromEntries(Object.entries(obj).filter(predicate)),
 	get tasksList() { return getFiles(tasksPath, excludeTasks); },
 	get nodePath() { return this.args.$node; },
