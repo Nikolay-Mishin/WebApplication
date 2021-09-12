1e7bc56ba166f7dda7c2b75e60e961da76b1309b
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-12 22:08:07 +0300

    Fix

--- tasks/helpers/tasks.js
+++ tasks/helpers/tasks.js
@@ -1,7 +1,6 @@
 const { tasksList, config: { tasksPath } } = require('./helpers');
 
 module.exports = process.node_tasks = process.node_tasks || (function tasks() {
-	if (process.node_tasks) return process.node_tasks;
 	const tasks = {};
 	tasksList.forEach(task => tasks[task.replace(/\-+/g, '_')] = require(`${tasksPath}/${task}.js`));
 	return process.node_tasks = tasks;
