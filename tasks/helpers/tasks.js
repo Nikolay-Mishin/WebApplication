6e888bfb163bf0facf9ac8aa3c6f2d39ffe95c45
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-12 22:10:02 +0300

    Fix

--- tasks/helpers/tasks.js
+++ tasks/helpers/tasks.js
@@ -1,13 +1,8 @@
 import { pathToFileURL as toUrl } from 'url';
 import h from './helpers.js';
-const {
-	tasksList,
-	config: { tasksPath },
-	modules: { path: { basename: base } }
-} = h;
+const { tasksList, config: { tasksPath } } = h;
 
 export default process.node_tasks = process.node_tasks || await (async function tasks() {
-	if (process.node_tasks) return process.node_tasks;
 	const tasks = {};
 	tasksList.forEach(task => {
 		const path = toUrl(tasksPath); // _relative(import.meta, tasksPath);
