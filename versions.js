81f5ba62db3d68e3e08fed4e2888a8a9bba436db
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-15 18:39:24 +0300

    Fix exports

--- versions.js
+++ versions.js
@@ -28,6 +28,7 @@
  * 4.6 - add GetContext() & runInContext() in Helpers
  * 5.0 - add multiProjectContext in gulpfile.configp
  * 5.1 - add Imports & ImportModules
+ * 5.2 = fix exports
 */
 
 /* new versions
