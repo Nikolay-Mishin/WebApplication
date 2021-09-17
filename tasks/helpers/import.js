88cc0d6c4857f3eab2cce460107c8854e17296af
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-17 16:07:42 +0300

    Fix imports

--- tasks/helpers/import.js
+++ tasks/helpers/import.js
@@ -1,24 +1,18 @@
 const { log } = require('console'),
-	{ basename: base, extname: ext } = require('path'),
-	{ concat, fromEntries, isObject, isDir, keys, values, getFiles } = require('./baseHelpers'),
-	$import = (toObj, ...modules) => {
-		const imports = concat(modules).map(m => {
-			const module = require(m);
-			return !toObj ? module : [base(m, ext(m)), module];
-		});
-		return !toObj ? imports : fromEntries(imports);
-	};
+	{ concat, fromEntries, isObject, isDir, keys, values, isArray, getFiles, fileName } = require('./baseHelpers'),
+	$import = (modules) => modules.map(m => require(m));
 
 // flatten arguments to enable both - imports([m1, m2]) or imports(m1, m2)
-const imports = (...modules) => $import(false, ...modules),
+const imports = (...modules) => $import(concat(modules)),
 	importModules = (...modules) => {
+		modules = concat(modules);
 		const isObj = isObject(modules[0]),
-			[_keys, _values, scan] = !isObj ? [[], [], isDir(modules[0])] : [keys(modules[0]), values(modules[0]), false],
-			_exclude = scan ? modules.pop() : [];
+			[_keys, _values, scan] = !isObj ? [[], [], isDir(modules[0])] : [keys(modules[0]), values(modules[0]), false];
 		modules = scan ? modules.shift() : (isObj ? _values : modules);
-		modules = (!scan ? modules : getFiles(modules, { _exclude })).map(m => !scan ? m : `${modules}/${m}`);
-		const imports = $import(true, ...modules);
-		return fromEntries(keys(imports).map((m, i) => [isObj ? _keys[i] : m.replace(/\-+/g, '_'), imports[m]]));
+		modules = (!scan ? modules : getFiles(modules, { exclude: scan && isArray(modules) ? modules.pop() : [] }))
+			.map(m => !scan ? m : `${modules}/${m}`);
+		const imports = $import(modules);
+		return fromEntries(imports.map((m, i) => [isObj ? _keys[i] : fileName(modules[i]).replace(/\-+/g, '_'), m]));
 	};
 
 module.exports = { imports, importModules };
