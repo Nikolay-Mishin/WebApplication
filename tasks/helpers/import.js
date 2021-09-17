813c2470e440581ea02a1e644eacf32170c02d83
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-17 16:30:02 +0300

    Fix importModules()

--- tasks/helpers/import.js
+++ tasks/helpers/import.js
@@ -1,26 +1,20 @@
 import { log } from 'console';
 import { pathToFileURL as toUrl } from 'url';
-import { basename as base, extname as ext } from 'path';
-import { concat, fromEntries, isObject, isDir, keys, values, getFiles } from './baseHelpers.js';
+import { concat, fromEntries, isObject, isDir, keys, values, isArray, getFiles, fileName } from './baseHelpers.js';
 
-const $import = async (toObj, ...modules) => {
-	const imports = await Promise.all(concat(modules).map(async m => {
-		const module = (await import(m)).default;
-		return !toObj ? module : [base(m, ext(m)), module];
-	}));
-	return !toObj ? imports : fromEntries(imports);
-};
+const $import = async modules => Promise.all(modules.map(async m => (await import(m)).default));
 
 // flatten arguments to enable both - imports([m1, m2]) or imports(m1, m2)
-export const imports = (...modules) => $import(false, ...modules),
+export const imports = (...modules) => $import(concat(modules)),
 	importModules = async (...modules) => {
+		modules = concat(modules);
 		const isObj = isObject(modules[0]),
-			[_keys, _values, scan] = !isObj ? [[], [], isDir(modules[0])] : [keys(modules[0]), values(modules[0]), false],
-			_exclude = scan ? modules.pop() : [];
+			[_keys, _values, scan] = !isObj ? [[], [], isDir(modules[0])] : [keys(modules[0]), values(modules[0]), false];
 		modules = scan ? modules.shift() : (isObj ? _values : modules);
-		modules = (!scan ? modules : getFiles(modules, { _exclude })).map(m => !scan ? m : `${toUrl(modules)}/${m}`);
-		const imports = await $import(true, ...modules);
-		return fromEntries(keys(imports).map((m, i) => [isObj ? _keys[i] : m.replace(/\-+/g, '_'), imports[m]]));
+		modules = (!scan ? modules : getFiles(modules, { exclude: scan && isArray(modules) ? modules.pop() : [] }))
+			.map(m => !scan ? m : `${toUrl(modules)}/${m}`);
+		return fromEntries((await $import(modules))
+			.map((m, i) => [isObj ? _keys[i] : fileName(modules[i]).replace(/\-+/g, '_'), m]));
 	};
 
 export default { imports, importModules };
