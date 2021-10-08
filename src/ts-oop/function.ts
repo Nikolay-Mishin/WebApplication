a584b9c2026c11f906db6118b929f6d3a586a06e
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-10-08 21:06:41 +0300

    Fix ts-oop examples

---
+++ src/ts-oop/function.ts
@@ -0,0 +1,14 @@
+// Отличие процедурного подхода от объектно-ориентированного
+// Данные -> Программа -> Процедуры (функции) -> Результат
+
+// Данные
+const width = 5;
+const height = 10;
+
+// процедура - функция
+function calcRectArea(width, height) {
+	return width * height; // результат
+}
+
+// вызов функции
+calcRectArea(width, height);
---
+++ src/ts-oop/oop-extends.ts
@@ -0,0 +1,147 @@
+// ООП построено на 3 основных концепциях: Инкапсуляция, Наследование и Полиморфизм
+// 2. Наследование
+
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/super
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Classes/static
+// https://learn.javascript.ru/es-object
+// https://learn.javascript.ru/class
+
+// Наследование - это механизм расширения функционала (свойств/методов) класса-родителя (parent), путем наследования (перенимания) публичных (public) и наследуемых (protected) свойств и методов класса-родителя и добавленых новых свойств/методов в классе-потомке (children)
+// Данный механизм позволяет очень эффективно переиспользовать код, масштабировать и поддерживать его
+// Наследование осуществляется с помощью ключевого слова `extends` после имени класса и указания имени класса-родителя после ключего слова
+// У класса может быть только 1 класс-родитель
+
+// constructor - если не объявлен конструктор класса, то он наследуется по умолчанию от класса-родителя
+// super - метод, который вызывает конструктор класса-родителя, с целью не затереть его (переопределить)
+// super([arguments]); // вызов родительского конструктора.
+// super.functionOnParent([arguments]); // вызов родительского метода `functionOnParent`.
+// super.propertyOnParent; // обращение к родительскому свойству.
+// super должен быть вызван первым!
+// методы и свойства дочернего класса, объявленные с теми же именами, что и у класса-родителя, переопределяют (перезаписывают/затирают) их
+
+// базовый класс
+class Person {
+
+	private _firstName;
+	private _lastName;
+	private _age;
+    
+	// инициализируем свойства в конструкторе класса
+	constructor(firstName, lastName, age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	// для каждого поля создаем геттеры и сеттеры
+
+	public get firstName() {
+		return this._firstName;
+	}
+
+	public set firstName(value) {
+		this._firstName = value;
+	}
+
+	public get lastName() {
+		return this._lastName;
+	}
+
+	public set lastName(value) {
+		this._lastName = value;
+	}
+
+	public get age() {
+		return this._age;
+	}
+
+	public set age(value) {
+		if (value <= 0) {
+			this._age = 0;
+		}
+		else {
+			this._age = value;
+		}
+	}
+
+	public get fullName() {
+		return `Фамилия - ${this.lastName} Имя - ${this.firstName}`;
+	}
+
+}
+
+// класс Работник - наследует класс Person (класс-родитель)
+class Employee extends Person {
+
+	private _inn;
+	private _number;
+	private _snils;
+
+	// инициализируем свойства в конструкторе класса=
+	constructor(firstName, lastName, age, inn, number, snils) {
+		super(firstName, lastName, age); // вызываем конструктор класса-родителя, чтобы его не затереть (переопределить)
+		this.inn = inn;
+		this.number = number;
+		this.snils = snils;
+	}
+
+	public get inn() {
+		return this._inn;
+	}
+
+	public set inn(value) {
+		this._inn = value;
+	}
+
+	public get number() {
+		return this._number;
+	}
+
+	public set number(value) {
+		this._number = value;
+	}
+
+	public get snils() {
+		return this._snils;
+	}
+
+	public set snils(value) {
+		this._snils = value;
+	}
+
+}
+
+const employee1 = new Employee('Ulbi', 'TV', 15, 15, 15, 15);
+console.log(employee1);
+
+class Developer extends Employee {
+
+	private _level;
+	private _language;
+    
+	constructor(firstName, lastName, age, inn, number, snils, level, language) {
+		super(firstName, lastName, age, inn, number, snils); // вызываем родительский конструктор
+		this.level = level;
+		this.language = language;
+	}
+
+	public get level() {
+		return this._level;
+	}
+
+	public set level(value) {
+		this._level = value;
+	}
+
+	public get language() {
+		return this._language;
+	}
+
+	public set language(value) {
+		this._language = value;
+	}
+
+}
+
+const developer1 = new Developer('Ulbi', 'TV', 15, 15, 15, 15, 'Senior', 'JavaScript');
+console.log(developer1);
